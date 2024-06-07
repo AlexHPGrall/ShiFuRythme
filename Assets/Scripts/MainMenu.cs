49a87ddd2823ae4d5e2c70aa5a665f6b6b2b2cc6
zac <109383107+Zac-oihfdwrrtuinvbcp@users.noreply.github.com>
2024-06-07 14:02:49 +0200

    fix

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("SongScanning");
     }
 
     public void OpenOptions()
